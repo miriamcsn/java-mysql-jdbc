8faca232490ff1313bb10572ecbc87c1e7cb6a26
miriamcsn <mceli87@gmail.com>
2021-07-31 14:45:21 -0300

    Upload code files already tested and running :)

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -17,10 +17,6 @@ public class Program {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-//		Department obj = new Department(1, "Books");
-//		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-//		System.out.println(obj);
-
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
